005c7029e89794b1248f61ec09269b33bc4d0fbb
Akshay Dharmavaram <akshaydharmavaram@gmail.com>
2018-07-22 22:57:10 +0530

    lot of changes

---
+++ _site/helloThere/helperFn.py
@@ -0,0 +1,21 @@
+def greetings(robot):
+    if(robot==False):
+        print("\nHi, my name is Akshay and this is my website.\n\n")
+
+
+def introduce(page):
+    print(page+"\n\n")
+
+def getPage():
+    return "I am an undergrad who can't resist playing around with computers.\nHowever, outside of work, I am an avid comedy and sci-fi fan."
+
+def hobbies():
+    print("My projects are presently just hobbies, but I plan to turn some of them into businesses someday.\n\n")
+
+def coolStuff():
+    print("I live in a dangerous loop where I take inspiration from sci-fi and comedy, and funnel them into my projects, despite limited success (for now).\n\n")
+
+def willDo():
+    print("I am presently coding up an AI, but it is taking longer than expected.")
+def showPage():
+    print("While I try to figure that out, why not have a look around?\n")
---
+++ helloThere/generalKenobi.py
@@ -0,0 +1,12 @@
+from helperFn import *
+
+assumingRobot=False
+greetings(assumingRobot)
+
+myPage=getPage()
+introduce(myPage)
+coolStuff()
+hobbies()
+
+willDo()
+showPage()
